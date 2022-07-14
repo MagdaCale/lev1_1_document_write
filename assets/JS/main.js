bf01e2da972c39dc33b5e2ef56b60b28aa7b102a
Magdalena Cale <107847566+MagdaCale@users.noreply.github.com>
2022-07-14 15:08:38 +0200

    lev1.1 document.write

---
+++ assets/JS/main.js
@@ -0,0 +1,16 @@
+document.write("<h1>Hello World!</h1>")
+document.write("<p>Have a nice day!</p>")
+
+let myText = "Hello again!"
+document.write("Hello again!")
+document.write("<br>")
+
+let firtName = 'Magnus'
+let lastName = 'Pool'
+document.write('<br>')
+
+document.write('Name: ' + firtName)
+document.write('<br>')
+document.write('Nachnme: ' + lastName)
+
+
